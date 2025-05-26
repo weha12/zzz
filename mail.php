26232d04124b6db19785b1356aa9dc81b0419941
weha12 <ohayotalaga@gmail.com>
2025-05-26 22:43:53 +0800

    hite

--- mail.php
+++ mail.php
@@ -3,9 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'C:\xampp\htdocs\yyy\PHPMailer\PHPMailer\src\Exception.php';
-require 'C:\xampp\htdocs\yyy\PHPMailer\PHPMailer\src\PHPMailer.php';
-require 'C:\xampp\htdocs\yyy\PHPMailer\PHPMailer\src\SMTP.php';
+require 'PHPMailer/PHPMailer/src/Exception.php';
+require 'PHPMailer/PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/PHPMailer/src/SMTP.php';
 
 function sendOtpEmail($email, $otp) {
     $mail = new PHPMailer(true);
